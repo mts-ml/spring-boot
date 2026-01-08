a4a4882ac541d4ea4067790ec6b134c63d8217df
Mateus Lima <mtsmlima@gmail.com>
2026-01-08 16:03:07 -0300

    refactor(User): Implement association with Order entity and add id to constructor

--- src/main/java/com/mateuslima/spring_boot/entities/User.java
+++ src/main/java/com/mateuslima/spring_boot/entities/User.java
@@ -3,6 +3,8 @@ package com.mateuslima.spring_boot.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -17,11 +19,15 @@ public class User implements Serializable {
     private String phone;
     private String password;
 
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+
 
     public User() {
     }
 
-    public User(String name, String email, String phone, String password) {
+    public User(Long id, String name, String email, String phone, String password) {
+        this.id = id;
         this.name = name;
         this.email = email;
         this.phone = phone;
@@ -63,6 +69,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
 
