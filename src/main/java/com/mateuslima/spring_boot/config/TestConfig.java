86b490117755285ce5edb030d479324e1ad91d7d
Mateus Lima <mtsmlima@gmail.com>
2026-01-08 16:59:38 -0300

    refactor(TestConfig): Add test to Order

--- src/main/java/com/mateuslima/spring_boot/config/TestConfig.java
+++ src/main/java/com/mateuslima/spring_boot/config/TestConfig.java
@@ -1,12 +1,15 @@
 package com.mateuslima.spring_boot.config;
 
+import com.mateuslima.spring_boot.entities.Order;
 import com.mateuslima.spring_boot.entities.User;
+import com.mateuslima.spring_boot.repositories.OrderRepository;
 import com.mateuslima.spring_boot.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 
@@ -19,6 +22,9 @@ public class TestConfig implements CommandLineRunner {
     // Quando rodar a aplicação o Spring resolve a dependência e associa uma instância de UserRepository aqui dentro
     private UserRepository userRepository;
 
+    @Autowired
+    private OrderRepository orderRepository;
+
 
     @Override
     public void run(String... args) throws Exception {
@@ -26,6 +32,11 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
-        userRepository.saveAll(Arrays.asList(u1, u2));
+        Order o1 = new Order(null, Instant.parse("2025-06-20T19:53:07Z"), u1);
+        Order o2 = new Order(null, Instant.parse("2025-07-21T03:42:10Z"), u2);
+        Order o3 = new Order(null, Instant.parse("2025-07-22T15:21:22Z"), u1);
+
+        userRepository.saveAll((Arrays.asList(u1, u2)));
+        orderRepository.saveAll(Arrays.asList(o1, o2, o3));
     }
 }
