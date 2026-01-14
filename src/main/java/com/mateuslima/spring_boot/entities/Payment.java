c0909ac1780ed9c8f6307cb1438d0dee131d6a41
Mateus Lima <mtsmlima@gmail.com>
2026-01-14 13:28:49 -0300

    fix(Payment): Add annotation @JsonIgnore to getOrders()

--- src/main/java/com/mateuslima/spring_boot/entities/Payment.java
+++ src/main/java/com/mateuslima/spring_boot/entities/Payment.java
@@ -1,5 +1,6 @@
 package com.mateuslima.spring_boot.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -42,6 +43,7 @@ public class Payment implements Serializable {
         this.moment = moment;
     }
 
+    @JsonIgnore
     public Order getOrder() {
         return order;
     }
