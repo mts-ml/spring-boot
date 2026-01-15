d5d83f9e367e5542c3b58c2e5aa38d531065e16b
Mateus Lima <mtsmlima@gmail.com>
2026-01-15 15:51:01 -0300

    feat(User): Implement delete()

--- src/main/java/com/mateuslima/spring_boot/resources/UserResources.java
+++ src/main/java/com/mateuslima/spring_boot/resources/UserResources.java
@@ -40,4 +40,11 @@ public class UserResources {
 
         return ResponseEntity.created(uri).body(obj);
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        userService.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
 }
